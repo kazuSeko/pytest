9f8bc8d2a2c6825e9dec22b72764d0ec7ce5e7ab
John <John@gmail.com>
2026-03-05 13:25:11 +0900

    First commit

---
+++ main-test.py
@@ -0,0 +1,10 @@
+from func_add import add
+
+def test_add_success():
+    assert add(2,4) == 6
+
+def test_add_negative():
+    assert add(-2,-4) == -6
+
+def test_add_zero():
+    assert add(0,0) == 0
---
+++ main.py
@@ -0,0 +1,6 @@
+# from func_add import add
+import func_add
+
+if __name__ == "__main__":
+    result = func_add.add(2,4)
+    print("result:",result)
